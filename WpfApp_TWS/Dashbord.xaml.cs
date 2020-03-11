35ac1d29a665423bdef0f718a7d25b7e72860bf5
Arthit <tgbroadcast@gmail.com>
2020-03-11 18:03:24 +0700

    Frist commit

---
+++ WpfApp_TWS/Dashbord.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+namespace WpfApp_TWS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Dashbord : Window
+    {
+        public Dashbord()
+        {
+            InitializeComponent();
+            PointShapeLineExample();
+
+            SeriesCollection = new SeriesCollection
+            {
+                new PieSeries
+                {
+                    Title = "ขนาด 4 กก.",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(8) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "ขนาด 7 กก.",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(6) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "ขนาด 11 กก.",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(10) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "ขนาด 11.5 กก.",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(4) },
+                    DataLabels = true
+                }
+            };
+            Chart1.LegendLocation = LiveCharts.LegendLocation.Bottom;
+            //adding values or series will update and animate the chart automatically
+            //SeriesCollection.Add(new PieSeries());
+            //SeriesCollection[0].Values.Add(5);
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+
+        private void UpdateAllOnClick(object sender, RoutedEventArgs e)
+        {
+            var r = new Random();
+
+            foreach (var series in SeriesCollection)
+            {
+                foreach (var observable in series.Values.Cast<ObservableValue>())
+                {
+                    observable.Value = r.Next(0, 10);
+                }
+            }
+        }
+
+        private void AddSeriesOnClick(object sender, RoutedEventArgs e)
+        {
+            var r = new Random();
+            var c = SeriesCollection.Count > 0 ? SeriesCollection[0].Values.Count : 5;
+
+            var vals = new ChartValues<ObservableValue>();
+
+            for (var i = 0; i < c; i++)
+            {
+                vals.Add(new ObservableValue(r.Next(0, 10)));
+            }
+
+            SeriesCollection.Add(new PieSeries
+            {
+                Values = vals
+            });
+        }
+
+        private void RemoveSeriesOnClick(object sender, RoutedEventArgs e)
+        {
+            if (SeriesCollection.Count > 0)
+                SeriesCollection.RemoveAt(0);
+        }
+
+        private void AddValueOnClick(object sender, RoutedEventArgs e)
+        {
+            var r = new Random();
+            foreach (var series in SeriesCollection)
+            {
+                series.Values.Add(new ObservableValue(r.Next(0, 10)));
+            }
+        }
+
+        private void RemoveValueOnClick(object sender, RoutedEventArgs e)
+        {
+            foreach (var series in SeriesCollection)
+            {
+                if (series.Values.Count > 0)
+                    series.Values.RemoveAt(0);
+            }
+        }
+
+        private void RestartOnClick(object sender, RoutedEventArgs e)
+        {
+            Chart1.Update(true, true);
+        }
+
+        public void PointShapeLineExample()
+        {           
+            SeriesCollection1 = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                },
+                new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+                    PointGeometry = null
+                },
+                new LineSeries
+                {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                    PointGeometry = DefaultGeometries.Square,
+                    PointGeometrySize = 15
+                }
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            //modifying the series collection will animate and update the chart
+            SeriesCollection1.Add(new LineSeries
+            {
+                Title = "Series 4",
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
+                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
+                PointGeometrySize = 50,
+                PointForeground = Brushes.Gray
+            });
+
+            //modifying any series values will also animate and update the chart
+            SeriesCollection1[3].Values.Add(5d);
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection1 { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+    }   
+}
